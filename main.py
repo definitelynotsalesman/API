baadc3d1cb9338b95374b197f9deb02eed77a01b
definitelynotkevin <definitelynotkevincs@gmail.com>
2025-03-11 16:35:05 -0500

    basic query for llama llm

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from meta_ai_api import MetaAI
 
 app = FastAPI()
+llama_model = MetaAI()
+
+origins = [
+    "http://localhost:3000",
+    "http://0.0.0.0:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+""" Query("Hello") -> result
+{
+  "response": {
+    "message": "Hello again! How's your day going so far?\n",
+    "sources": [],
+    "media": []
+  }
+}
+"""
+@app.get("/llama/{query}") #fix this garbage
+async def pass_query(query: str):
+    return {"response": llama_model.prompt(query)}
+
+
+    
